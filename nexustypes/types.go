11eafd46d9270f9986bd925b203382eb6796555b
Roey Berman <roey.berman@gmail.com>
2023-08-16 07:34:18 -0600

    Cleanup

--- nexustypes/types.go
+++ nexustypes/types.go
@@ -5,19 +5,33 @@ import (
 )
 
 type (
+	// Payload is a generic data container.
 	Payload struct {
+		// A key-value mapping for additional context. Useful for decoding the 'data' field, if needed.
 		Metadata map[string]string `json:"metadata"`
-		Data     []byte            `json:"data"`
+		// Arbitrary data.
+		Data []byte `json:"data"`
 	}
 
-	// TODO: Find a better name for this
-	UntypedFailure struct {
-		Message any `json:"message"`
+	// Failure represents protocol level failures returned in non successful HTTP responses as well as `failed` or
+	// `canceled` operation results.
+	Failure struct {
+		// A simple text message.
+		Message string `json:"message"`
+		// Additional structured data. If this is byte data, it must be JSON serializable.
 		Details any `json:"details"`
 	}
 
-	Failure struct {
-		Message json.RawMessage `json:"message"`
+	// PayloadFailure is a variant of the Failure struct, useful for transferring encrypted or binary information.
+	// The Message field must be a Payload that can be decoded as string.
+	PayloadFailure struct {
+		Message Payload `json:"message"`
+		Details Payload `json:"details"`
+	}
+
+	// RawFailure is a variant of the Failure struct that contains raw (unparsed) details.
+	RawFailure struct {
+		Message string          `json:"message"`
 		Details json.RawMessage `json:"details"`
 	}
 
@@ -30,10 +44,8 @@ type (
 )
 
 const (
-	contentTransferEncoding      = "Content-Transfer-Encoding"
-	contentTransferEncodingLower = "content-transfer-encoding"
-	OperationStateRunning        = OperationState("running")
-	OperationStateSucceeded      = OperationState("succeeded")
-	OperationStateFailed         = OperationState("failed")
-	OperationStateCanceled       = OperationState("canceled")
+	OperationStateRunning   = OperationState("running")
+	OperationStateSucceeded = OperationState("succeeded")
+	OperationStateFailed    = OperationState("failed")
+	OperationStateCanceled  = OperationState("canceled")
 )
--- nexustypes/types_test.go
+++ nexustypes/types_test.go
@@ -10,14 +10,12 @@ import (
 
 func TestFailure_JSONMarshalling(t *testing.T) {
 	type testcase struct {
-		name       string
-		message    any
+		message    string
 		details    any
 		serialized string
 	}
 	cases := []testcase{
 		{
-			name:    "simple",
 			message: "simple",
 			details: "details",
 			serialized: `{
@@ -26,7 +24,6 @@ func TestFailure_JSONMarshalling(t *testing.T) {
 }`,
 		},
 		{
-			name:    "complex",
 			message: "complex",
 			details: struct {
 				M map[string]string
@@ -45,11 +42,35 @@ func TestFailure_JSONMarshalling(t *testing.T) {
 	}
 }`,
 		},
-		{
-			name:    "with payload",
-			message: Payload{Metadata: map[string]string{"a": "b"}, Data: []byte("message")},
-			details: Payload{Metadata: map[string]string{}, Data: []byte{0x00, 0x01}},
-			serialized: `{
+	}
+	for _, tc := range cases {
+		tc := tc
+		t.Run(tc.message, func(t *testing.T) {
+			source, err := json.MarshalIndent(Failure{tc.message, tc.details}, "", "\t")
+			require.NoError(t, err)
+			require.Equal(t, tc.serialized, string(source))
+
+			var failure RawFailure
+			err = json.Unmarshal(source, &failure)
+			require.NoError(t, err)
+
+			require.Equal(t, tc.message, failure.Message)
+
+			detailsPointer := reflect.New(reflect.TypeOf(tc.details)).Interface()
+			err = json.Unmarshal(failure.Details, detailsPointer)
+			details := reflect.ValueOf(detailsPointer).Elem().Interface()
+			require.NoError(t, err)
+			require.Equal(t, tc.details, details)
+		})
+	}
+}
+
+func TestPayloadFailure_JSONMarshalling(t *testing.T) {
+	failure := PayloadFailure{
+		Message: Payload{Metadata: map[string]string{"a": "b"}, Data: []byte("message")},
+		Details: Payload{Metadata: map[string]string{}, Data: []byte{0x00, 0x01}},
+	}
+	serialized := `{
 	"message": {
 		"metadata": {
 			"a": "b"
@@ -60,33 +81,17 @@ func TestFailure_JSONMarshalling(t *testing.T) {
 		"metadata": {},
 		"data": "AAE="
 	}
-}`,
-		},
-	}
-	for _, tc := range cases {
-		tc := tc
-		t.Run(tc.name, func(t *testing.T) {
-			source, err := json.MarshalIndent(UntypedFailure{tc.message, tc.details}, "", "\t")
-			require.NoError(t, err)
-			require.Equal(t, tc.serialized, string(source))
+}`
+	source, err := json.MarshalIndent(failure, "", "\t")
+	require.NoError(t, err)
+	require.Equal(t, serialized, string(source))
 
-			var failure Failure
-			err = json.Unmarshal(source, &failure)
-			require.NoError(t, err)
+	var decoded PayloadFailure
+	err = json.Unmarshal(source, &decoded)
+	require.NoError(t, err)
 
-			messagePointer := reflect.New(reflect.TypeOf(tc.message)).Interface()
-			err = json.Unmarshal(failure.Message, messagePointer)
-			message := reflect.ValueOf(messagePointer).Elem().Interface()
-			require.NoError(t, err)
-			require.Equal(t, tc.message, message)
-
-			detailsPointer := reflect.New(reflect.TypeOf(tc.details)).Interface()
-			err = json.Unmarshal(failure.Details, detailsPointer)
-			details := reflect.ValueOf(detailsPointer).Elem().Interface()
-			require.NoError(t, err)
-			require.Equal(t, tc.details, details)
-		})
-	}
+	require.Equal(t, failure.Message, decoded.Message)
+	require.Equal(t, failure.Details, decoded.Details)
 }
 
 func TestOperationInfo_JSONMarshalling(t *testing.T) {
