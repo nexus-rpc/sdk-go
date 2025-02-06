17aba31fd502733d795b988edcfc20f895315426
Roey Berman <roey.berman@gmail.com>
2025-02-06 13:14:17 -0800

    Add test for handler context

---
+++ nexus/handler_context_test.go
@@ -0,0 +1,21 @@
+package nexus_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/nexus-rpc/sdk-go/nexus"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHandlerContext(t *testing.T) {
+	ctx := nexus.WithHandlerContext(context.Background())
+	require.True(t, nexus.IsHandlerContext(ctx))
+	initial := []nexus.Link{{Type: "foo"}, {Type: "bar"}}
+	nexus.AddHandlerLinks(ctx, initial...)
+	additional := nexus.Link{Type: "baz"}
+	nexus.AddHandlerLinks(ctx, additional)
+	require.Equal(t, append(initial, additional), nexus.HandlerLinks(ctx))
+	nexus.SetHandlerLinks(ctx, initial...)
+	require.Equal(t, initial, nexus.HandlerLinks(ctx))
+}
